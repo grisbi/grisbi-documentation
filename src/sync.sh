74d2f3f60779c30caf236134710a2b3c5e89720f
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-02-11 17:28:40 +0100

    sync: upload the English manual

--- src/sync.sh
+++ src/sync.sh
@@ -6,3 +6,8 @@ set -e
 pushd $(dirname $0)/html_img
 rsync --recursive --verbose --update .  ludov@web.sourceforge.net:/home/project-web/grisbi/htdocs/html/
 popd
+
+# Update the English manual
+pushd $(dirname $0)/html_img-en
+rsync --recursive --verbose --update .  ludov@web.sourceforge.net:/home/project-web/grisbi/htdocs/html-en/
+popd
